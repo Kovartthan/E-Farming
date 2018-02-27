cf255d17ef493d6a96471d40509b740cfb6d8710
kovartthan <kovartthan@github.com>
2018-02-27 19:17:22 +0530

    Login sign up partial commit

--- app/src/main/java/com/ko/efarming/ui/EFProgressDialog.java
+++ app/src/main/java/com/ko/efarming/ui/EFProgressDialog.java
@@ -2,22 +2,22 @@ package com.ko.efarming.ui;
 
 import android.app.Dialog;
 import android.content.Context;
-import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.graphics.drawable.DrawableCompat;
 import android.view.Window;
 import android.widget.ProgressBar;
 
-import com.ciceroneme.android.R;
+import com.ko.efarming.R;
 
-public class CeProgressDialog extends Dialog {
+
+public class EFProgessDialog extends Dialog {
 
     private Context mContext;
     ProgressBar progressBar;
 
-    public CeProgressDialog(Context context) {
-        super(context,R.style.TransparentProgressDialog);
+    public EFProgessDialog(Context context) {
+        super(context, R.style.TransparentProgressDialog);
         this.mContext = (context).getApplicationContext();
     }
 
@@ -28,10 +28,10 @@ public class CeProgressDialog extends Dialog {
         setContentView(R.layout.dialog_custom_progress_loader);
         progressBar = (ProgressBar) findViewById(R.id.progress);
         Drawable normalDrawable = mContext.getResources().getDrawable(R.drawable.progress_diag_spinner_anim);
-        Drawable wrapDrawable = DrawableCompat.wrap(normalDrawable);
-        DrawableCompat.setTint(wrapDrawable, mContext.getResources().getColor(R.color.progress_color));
+//        Drawable wrapDrawable = DrawableCompat.wrap(normalDrawable);
+//        DrawableCompat.setTint(wrapDrawable, mContext.getResources().getColor(R.color.progress_color));
         this.setCancelable(false);
-        progressBar.setIndeterminateDrawable(wrapDrawable);
+        progressBar.setIndeterminateDrawable(normalDrawable);
     }
 
 
