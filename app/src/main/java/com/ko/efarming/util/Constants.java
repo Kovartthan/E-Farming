fc5ba679b36454a8e343ff08131c66682cf6d483
kovartthan <kovartthan@github.com>
2018-02-27 18:57:38 +0530

    Login sign up partial commit

--- app/src/main/java/com/ko/efarming/util/Constants.java
+++ app/src/main/java/com/ko/efarming/util/Constants.java
@@ -8,9 +8,6 @@ import android.Manifest;
 
 public interface Constants {
     public static String USERS = "users";
-    public static String CHAT_ROOMS = "chat_rooms";
-    public String GET_CHAT_OBJECT = "get_chat_object";
-    public String ONLINE_STATUS = "online_status";
     String[] PERMISSIONS = {Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE};
     int REQUEST_PERMISSION_READ_STORAGE = 2;
     int REQUEST_PICTURE_FROM_GALLERY = 200;
